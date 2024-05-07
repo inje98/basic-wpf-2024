57e5cc078163fe4b2a60fd5bd046f2dabafbcfe9
inje98 <kij980723@gmail.com>
2024-05-07 17:30:05 +0900

    5일차

---
+++ day05/wpf05_mvvm_app/ex07_EmployeeMngApp/Views/MainView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows; // 원래 이거였는데 이제 필요없지
+using ex07_EmployeeMngApp.Helpers;
+using MahApps.Metro.Controls; // 이거 넣어야됨
+using MahApps.Metro.Controls.Dialogs;
+namespace ex07_EmployeeMngApp.Views
+{
+    /// <summary>
+    /// MainView.xaml에 대한 상호 작용 논리
+    /// 여기는 프로그래밍 코딩은 안함
+    /// </summary>
+    public partial class MainView : MetroWindow // MetroWindow로 바꾸고
+    {
+        public MainView()
+        {
+            InitializeComponent();
+
+            Common.DialogCoordinator = DialogCoordinator.Instance; // 생성된 다이얼로그 꾸미기 객체를 공통으로 이전
+            this.DataContext = DialogCoordinator.Instance;
+        }
+    }
+}
---
+++ day05/wpf05_mvvm_app/ex10_MovieFinder2024/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Windows;
+using System.Windows.Controls;
+
+using System.Windows.Input;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace ex10_MovieFinder2024
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            // await this.ShowMessageAsync("검색", "검색을 시작합니다");
+
+
+            if (string.IsNullOrEmpty(TxtMovieName.Text))
+            {
+                await this.ShowMessageAsync("검색", "검색할 영화명을 입력하세요!");
+                return;
+            }
+
+            SearchMovie(TxtMovieName.Text);
+        }
+
+        private async void SearchMovie(string movieName)
+        {
+            string tmdb_apiKey = "d768e2483eefaeb736d4bb6f565365be"; // TMDB사이트에서 제공받은 API키.
+            string encoding_movieName = HttpUtility.UrlEncode(movieName, Encoding.UTF8);
+            string openApiUri = $"https://api.themoviedb.org/3/search/movie?api_key={tmdb_apiKey}" +
+                                $"&language=ko-KR&page=1&include_adult=false&query={encoding_movieName}";
+            //Debug.WriteLine(openApiUri);
+
+            string result = string.Empty;
+
+            WebRequest req = null;
+            WebResponse res = null;
+            StreamReader reader = null;
+
+            try
+            {
+                req = WebRequest.Create(openApiUri);
+                res = await req.GetResponseAsync();
+                reader = new StreamReader(res.GetResponseStream());
+                result = reader.ReadToEnd();
+
+                Debug.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"{ex.Message}");
+            }
+
+        }
+
+
+
+        private void TxtMovieName_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private async void GrdResult_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            await this.ShowMessageAsync("포스터", "포스터처리합니다");
+        }
+
+        private async void BtnAddFavorite_Click(object sender, RoutedEventArgs e)
+        {
+            await this.ShowMessageAsync("즐겨찾기", "즐겨찾기 추가합니다.");
+        }
+
+        private async void BtnViewFavorite_Click(object sender, RoutedEventArgs e)
+        {
+            await this.ShowMessageAsync("즐겨찾기", "즐겨찾기 확인합니다.");
+
+        }
+
+        private async void BtnDelFavorite_Click(object sender, RoutedEventArgs e)
+        {
+            await this.ShowMessageAsync("즐겨찾기", "즐겨찾기 삭제합니다.");
+        }
+
+        private async void BtnWatchTrailer_Click(object sender, RoutedEventArgs e)
+        {
+            await this.ShowMessageAsync("유튜브예고편", "예고편 확인합니다");
+        }
+
+    }
+}
