6708990e4d9e7ffd60d9e0c1b62c0399302fdccb
inje98 <kij980723@gmail.com>
2024-05-09 17:28:50 +0900

    완료

--- day07/wpf07_openapi_app/ex11_Gimhae_FindDust/MainWindow.xaml.cs
+++ day07/wpf07_openapi_app/ex11_Gimhae_FindDust/MainWindow.xaml.cs
@@ -1,15 +1,8 @@
 ﻿using System.IO;
 using System.Net;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ex11_Gimhae_FindDust.Models;
 using MahApps.Metro.Controls; //스킨
 using MahApps.Metro.Controls.Dialogs; //스킨
